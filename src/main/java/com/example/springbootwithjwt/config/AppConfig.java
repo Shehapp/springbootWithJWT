0494e4c81e5a625b7e649b64fb0dfd84537a5fad
shehap <shehap@gmail.com>
2023-09-20 23:11:56 +0300

    add DaoProvider and PasswordEncoder

---
+++ src/main/java/com/example/springbootwithjwt/config/AppConfig.java
@@ -0,0 +1,28 @@
+package com.example.springbootwithjwt.config;
+
+import com.example.springbootwithjwt.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@AllArgsConstructor
+public class AppConfig {
+
+    private final UserDetailsService userDetailsService;
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider=new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService);
+        return provider;
+    }
+}
