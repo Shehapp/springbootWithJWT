a75b443aeacff6a8f704a6b32ba3f89993328eda
shehap <shehap@gmail.com>
2023-09-20 21:29:15 +0300

    init commit

---
+++ src/main/java/com/example/springbootwithjwt/SpringbootWithJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootwithjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithJwtApplication.class, args);
+	}
+
+}
